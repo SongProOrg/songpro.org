f437edcc79737accf02d640cc6365e04b8381a89
Brian Kelly <polymonic@gmail.com>
2018-10-01 22:57:54 -0400

    Switch to extracted songpro-editor-component

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,45 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {SongPro} from "./song_pro/SongPro";
+import SongProEditor from 'songpro-editor-component';
 
 function renderEditor(id) {
   const el = document.getElementById(id);
-  ReactDOM.render(<App />, el);
+  ReactDOM.render(<SongProEditor />, el);
 }
 
 export default {
   renderEditor
 }
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      previewHtml: '',
-    };
-
-    this.sourceChanged = this.sourceChanged.bind(this);
-  }
-
-  sourceChanged(event) {
-    const song = SongPro.parse(event.target.value);
-    this.setState({
-      previewHtml: song.toHtml(),
-    })
-  }
-
-  render () {
-    return (
-      <div className="app">
-        <div className="editor">
-          <textarea onChange={this.sourceChanged}/>
-        </div>
-        <div className="preview">
-          { this.state.previewHtml }
-        </div>
-      </div>
-    )
-  }
-}
-
