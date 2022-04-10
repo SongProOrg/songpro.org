539b20003fdf760ee37190d49033649bad2b5339
Brian Kelly <polymonic@gmail.com>
2022-04-10 10:53:27 -0500

    Remove unused JavaScript

--- config.rb
+++ config.rb
@@ -1,12 +1,4 @@
 activate :directory_indexes
 
-activate :external_pipeline,
-  name: :webpack,
-  command: build? ?
-  "NODE_ENV=production ./node_modules/webpack/bin/webpack.js --bail -p" :
-  "./node_modules/webpack/bin/webpack.js --watch -d --progress --color",
-  source: "tmp/dist",
-  latency: 1
-
 set :markdown_engine, :redcarpet
 set :markdown, fenced_code_blocks: true, smartypants: true
